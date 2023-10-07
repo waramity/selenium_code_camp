52115663102be4b0229572fe532444f8e657fcbd
unknown <waramity.re@gmail.com>
2023-10-07 15:49:53 +0700

    add fill text in input function

--- main.py
+++ main.py
@@ -1,16 +1,24 @@
 from selenium import webdriver
 import time
+from selenium.webdriver.common.keys import Keys
 
 options = webdriver.ChromeOptions()
-options.add_experimental_option("detach", True)
+options.add_experimental_option('detach', True)
 driver = webdriver.Chrome(options=options)
 
-driver.get("https://www.saucedemo.com/")
-# driver.implicitly_wait(3) 
+driver.get('https://www.saucedemo.com/')
+driver.implicitly_wait(5) 
+
+user_name_field = driver.find_element('id', 'user-name')
+user_name_field.send_keys("kuy")
+
+password_field = driver.find_element('id', 'password')
+password_field.send_keys("sus")
 
 # login_button = driver.find_element_by_id('login-button')
-login_button = driver.find_element("id", 'login-button')
+login_button = driver.find_element('id', 'login-button')
 login_button.click()
+
 
 time.sleep(2)
 
