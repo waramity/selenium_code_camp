b35fe626e6024d775ee630118dc9df8c9bf2ff6d
unknown <waramity.re@gmail.com>
2023-10-07 16:16:48 +0700

    finish scrape sauce demo

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from selenium import webdriver
 import time
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+
 
 options = webdriver.ChromeOptions()
 options.add_experimental_option('detach', True)
@@ -9,16 +11,49 @@ driver = webdriver.Chrome(options=options)
 driver.get('https://www.saucedemo.com/')
 driver.implicitly_wait(5) 
 
-user_name_field = driver.find_element('id', 'user-name')
-user_name_field.send_keys("kuy")
+time.sleep(2)
+username_field = driver.find_element(By.ID, 'user-name')
+username_field.send_keys('standard_user')
 
-password_field = driver.find_element('id', 'password')
-password_field.send_keys("sus")
+time.sleep(2)
+password_field = driver.find_element(By.ID, 'password')
+password_field.send_keys('secret_sauce')
 
-# login_button = driver.find_element_by_id('login-button')
-login_button = driver.find_element('id', 'login-button')
+time.sleep(2)
+login_button = driver.find_element(By.ID, 'login-button')
 login_button.click()
 
+time.sleep(2)
+add_to_cart_button = driver.find_element(By.ID, 'add-to-cart-sauce-labs-backpack')
+add_to_cart_button.click()
+
+time.sleep(2)
+cart_button = driver.find_element(By.CLASS_NAME, 'shopping_cart_link')
+cart_button.click()
+
+time.sleep(2)
+checkout_button = driver.find_element(By.ID, 'checkout')
+checkout_button.click()
+
+time.sleep(2)
+first_name_field = driver.find_element(By.ID, 'first-name')
+first_name_field.send_keys('test')
+
+time.sleep(2)
+last_name_field = driver.find_element(By.ID, 'last-name')
+last_name_field.send_keys('test2')
+
+time.sleep(2)
+postal_field = driver.find_element(By.ID, 'postal-code')
+postal_field.send_keys('11111')
+
+time.sleep(2)
+continue_button = driver.find_element(By.ID, 'continue')
+continue_button.click()
+
+time.sleep(2)
+finish_button = driver.find_element(By.ID, 'finish')
+finish_button.click()
 
 time.sleep(2)
 
